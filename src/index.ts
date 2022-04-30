871773e7872a37ebae37653e136d1dd920eda5c5
coloredwax <crypticdiscord@gmail.com>
2022-04-30 00:23:20 -0400

    Fixed typecasting for variants

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 interface Parameters<Props, Propless> {
   base: string,
-  variants: Propless extends null ? Partial<{ [Key in keyof Props]: Props[Key] extends string | undefined ? Partial<{ [Value in Extract<Props[Key], string>]: string }> : string }> : Propless,
-  compounds: Array<[Partial<Propless extends null ? Props : { [Key in keyof Propless]: Propless[Key] extends object ? keyof Propless[Key] : boolean }>, string]>
+  variants?: Propless extends null ? Partial<{ [Key in keyof Props]: Props[Key] extends string | undefined ? Partial<{ [Value in Extract<Props[Key], string>]: string }> : string }> : Propless,
+  compounds?: Array<[Partial<Propless extends null ? Props : { [Key in keyof Propless]: Propless[Key] extends object ? keyof Propless[Key] : boolean }>, string]>
 }
 
 const stitchtail = <Props = null, Propless = null>({ base, variants, compounds }: Parameters<Props, Propless>) => {
@@ -9,27 +9,29 @@ const stitchtail = <Props = null, Propless = null>({ base, variants, compounds }
 		const classes = [...base.split(" ")];
 
 		if (props) {
-			Object.entries(props).forEach(([key, value]) => {
-				if (variants[key as keyof typeof variants] && value) {
-					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-					// @ts-ignore
-					classes.push(...((typeof variants[key as keyof typeof variants] === "string" ? variants[key as keyof typeof variants] : variants[key as keyof typeof variants][value]) as string).split(" "));
-				}
-			});
+			if (variants) {
+				Object.entries(props).forEach(([key, value]) => {
+					let vari: unknown = variants as unknown;
 
-			for (let i = 0; i < compounds.length; i++) {
-				const entries = Object.entries(compounds[i][0]);
-				let matches = 0;
-
-				entries.forEach(([key, value]) => {
-					props[key as keyof typeof props] === value && matches++;
+					classes.push(...((typeof variants[key as keyof typeof variants] === "string" ? variants[key as keyof typeof variants] : variants[key as keyof typeof variants][value as keyof { [key in keyof typeof vari]: typeof variants[key] }]) as string).split(" "));
 				});
+			};
 
-				if (entries.length === matches) {
-					classes.push(...compounds[i][1].split(" "));
-				}
-			}
-		}
+			if (compounds) {
+				for (let i = 0; i < compounds.length; i++) {
+					const entries = Object.entries(compounds[i][0]);
+					let matches = 0;
+
+					entries.forEach(([key, value]) => {
+						props[key as keyof typeof props] === value && matches++;
+					});
+
+					if (entries.length === matches) {
+						classes.push(...compounds[i][1].split(" "));
+					};
+				};				
+			};
+		};
 
 		return classes.filter(value => value.length > 0).join(" ").trim();
 	};
