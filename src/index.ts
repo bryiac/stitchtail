a87192297c233e97b2bbe49638e99bb2ffe51220
coloredwax <crypticdiscord@gmail.com>
2022-05-10 11:52:03 -0400

    Added defaults and fixed boolean values not being pushed properly

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 interface Parameters<Props, Propless> {
-	base: string,
+	base?: string,
 	variants?: Propless extends null ? Partial<{ [Key in keyof Props]: Props[Key] extends string | undefined ? Partial<{ [Value in Extract<Props[Key], string>]: string }> : string }> : Propless,
-	compounds?: Array<[Partial<Propless extends null ? Props : { [Key in keyof Propless]: Propless[Key] extends object ? keyof Propless[Key] : boolean }>, string]>
+	compounds?: Array<[Partial<Propless extends null ? Props : { [Key in keyof Propless]: Propless[Key] extends object ? keyof Propless[Key] : boolean }>, string]>,
+	defaults?: Partial<Propless extends null ? Props : { [Key in keyof Propless]: Propless[Key] extends object ? keyof Propless[Key] : boolean }>
 }
 
-const stitchtail = <Props = null, Propless = null>({ base, variants, compounds }: Parameters<Props, Propless>) => {
+const stitchtail = <Props = null, Propless = null>({ base, variants, compounds, defaults }: Parameters<Props, Propless>) => {
 	return (props?: Partial<Propless extends null ? Props : { [Key in keyof Propless]: Propless[Key] extends object ? keyof Propless[Key] : boolean }>) => {
-		const classes = [...base.split(" ")];
+		const classes = base ? [...base.split(" ")] : [];
 
 		if (props) {
 			if (variants) {
 				Object.entries(props).forEach(([key, value]) => {
 					const vari: unknown = variants as unknown;
 
-					if (variants[key as keyof typeof variants] && variants[key as keyof typeof variants][value as keyof { [key in keyof typeof vari]: typeof variants[key] }]) {
-						classes.push(...((typeof variants[key as keyof typeof variants] === "string" ? variants[key as keyof typeof variants] : variants[key as keyof typeof variants][value as keyof { [key in keyof typeof vari]: typeof variants[key] }]) as string).split(" "));
+					if (typeof variants[key as keyof typeof variants] === "string") {
+						classes.push(...(variants[key as keyof typeof variants] as string).split(" "));
+					} else if (variants[key as keyof typeof variants][value as keyof { [key in keyof typeof vari]: typeof variants[key] }]) {
+						classes.push(...(variants[key as keyof typeof variants][value as keyof { [key in keyof typeof vari]: typeof variants[key] }] as string).split(" "));
+					} else if (defaults && defaults[key as keyof typeof defaults]) {
+						classes.push(...(defaults[key as keyof typeof defaults] as string).split(" "));
 					}
 				});
 			}
